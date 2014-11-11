01798d0036b272fbd2888f5ba6bf7a2d7992f4f6
noise23 <admin@acidpool.com>
2014-11-12 00:07:27 +0200

    Set default cppolicy to strict

--- src/init.cpp
+++ src/init.cpp
@@ -244,7 +244,7 @@ std::string HelpMessage()
         "  -listen                " + _("Accept connections from outside (default: 1 if no -proxy or -connect)") + "\n" +
         "  -bind=<addr>           " + _("Bind to given address. Use [host]:port notation for IPv6") + "\n" +
         "  -dnsseed               " + _("Find peers using DNS lookup (default: 0)") + "\n" +
-		"  -cppolicy              " + _("Sync checkpoints policy (default: advisory)") + "\n" +
+		"  -cppolicy              " + _("Sync checkpoints policy (default: strict)") + "\n" +
         "  -banscore=<n>          " + _("Threshold for disconnecting misbehaving peers (default: 100)") + "\n" +
         "  -bantime=<n>           " + _("Number of seconds to keep misbehaving peers from reconnecting (default: 86400)") + "\n" +
         "  -maxreceivebuffer=<n>  " + _("Maximum per-connection receive buffer, <n>*1000 bytes (default: 5000)") + "\n" +
@@ -352,7 +352,7 @@ bool AppInit2()
     // ********************************************************* Step 2: parameter interactions
 
 		CheckpointsMode = Checkpoints::STRICT; 
-    std::string strCpMode = GetArg("-cppolicy", "advisory"); 
+    std::string strCpMode = GetArg("-cppolicy", "strict"); 
  
     if(strCpMode == "strict") 
         CheckpointsMode = Checkpoints::STRICT; 
